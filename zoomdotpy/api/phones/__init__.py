46d439a91ee62ad0cf55914919c68fada187d10d
Abdellah <abdellah.fytahi@karglobal.com>
2023-03-09 13:54:15 -0500

    added call_queues support

--- zoomdotpy/api/phones/__init__.py
+++ zoomdotpy/api/phones/__init__.py
@@ -2,6 +2,7 @@ from .sites import SitesAPI
 from .devices import DevicesAPI
 from .auto_receptionists import AutoReceptionistsAPI
 from .emergency_addresses import EmergencyAddressesAPI
+from .call_queues import CallQueuesAPI
 
 from zoomdotpy.api.base import _BaseAPI
 
@@ -10,8 +11,10 @@ class PhonesAPI(_BaseAPI):
     sites: SitesAPI
     auto_receptionists: AutoReceptionistsAPI
     emergency_addresses: EmergencyAddressesAPI
+    call_queues: CallQueuesAPI
 
-    def __post_init__(self):
+    def __post_init__(self):        
+        self.call_queues= CallQueuesAPI(self._s)
         self.devices    = DevicesAPI(self._s)
         self.sites      = SitesAPI(self._s)
         self.auto_receptionists     = AutoReceptionistsAPI(self._s)
--- zoomdotpy/api/phones/call_queues.py
+++ zoomdotpy/api/phones/call_queues.py
@@ -1,73 +1,145 @@
 from ..base import _BaseAPI
 
 class CallQueuesAPI(_BaseAPI):
-    def create_call_queues(self , callQueueId, policyType):
+    def list_call_queues(self, params: dict = {}):
         """
-            Use this API to add the policy sub-setting for a specific [call queue](https://support.zoom.us/hc/en-us/articles/360021524831) according to the `policyType`. For example, you can use this API to set up shared access members. 
+            Call queues allow you to route incoming calls to a group of users. For instance, you can use call queues to route calls to various departments in your organization such as sales, engineering, billing, customer service etc.
+			 Use this API to list Call queues.
 			
-			**Prerequisites:** 
-			* Pro or higher account with Zoom Phone license.
-			* Account owner or admin privileges 
+			
+			**Prerequisites:**
+			
+			* Pro, Business, or Education account
+			* Account owner or admin permissions
+			* Zoom Phone license
+			
+			**Scopes:** `phone:read:admin`
+			 
+			
+			
+			 **[Rate Limit Label](https://marketplace.zoom.us/docs/api-reference/rate-limits#rate-limits):** `Medium`
+			
+        """
+
+        res = self.request(
+            'GET',
+            f'/phone/call_queues',
+            params=params
+        )
+
+        return res.json()
+
+    def create_call_queue(self, body: dict):
+        """
+            Call queues allow you to route incoming calls to a group of users. For instance, you can use call queues to route calls to various departments in your organization such as sales, engineering, billing, customer service etc.
+			 Use this API to [create a call queue](https://support.zoom.us/hc/en-us/articles/360021524831-Managing-Call-Queues#h_e81faeeb-9184-429a-aaea-df49ff5ff413).
+			 You can add phone users or common area phones to call queues.
+			
+			**Prerequisites:**
+			
+			* Pro, Business, or Education account
+			* Account owner or admin permissions
+			* Zoom Phone license
 			
 			**Scopes:** `phone:write:admin`
+			 
+			
 			
 			 **[Rate Limit Label](https://marketplace.zoom.us/docs/api-reference/rate-limits#rate-limits):** `Light`
         """
-
-        # TBD
-        return
 
         res = self.request(
             'POST',
-            f'/phone/call_queues/{callQueueId}/policies/{policyType}'
+            f'/phone/call_queues',
+            json=body
         )
 
         return res.json()
-        
-        def update_call_queues(self , callQueueId, policyType):
+    
+    def delete_call_queue(self, call_queue_id: str) -> bool:
         """
-            Use this API to update the policy sub-setting for a specific [call queue](https://support.zoom.us/hc/en-us/articles/360021524831) according to the `policyType`. For example, you can use this API to update shared access members.
+            Call queues allow you to route incoming calls to a group of users. For instance, you can use call queues to route calls to various departments in your organization such as sales, engineering, billing, customer service etc.
+			 Use this API to delete a Call Queue.
+			 
+			**Prerequisites:**
 			
-			**Prerequisites:** 
-			* Pro or higher account with Zoom Phone license.
-			* Account owner or admin privileges 
+			* Pro, Business, or Education account
+			* Account owner or admin permissions
+			* Zoom Phone license
 			
 			**Scopes:** `phone:write:admin`
+			 
+			
 			
 			 **[Rate Limit Label](https://marketplace.zoom.us/docs/api-reference/rate-limits#rate-limits):** `Light`
+			
+			
         """
 
-        # TBD
-        return
-
-        res = self.request(
-            'PATCH',
-            f'/phone/call_queues/{callQueueId}/policies/{policyType}'
-        )
-
-        return res.json()
-        
-        def delete_call_queues(self , callQueueId, policyType):
-        """
-            Use this API to remove the policy sub-setting for a specific [call queue](https://support.zoom.us/hc/en-us/articles/360021524831) according to the `policyType`. For example, you can use this API to remove shared access members. 
-			
-			**Prerequisites:** 
-			* Pro or higher account with Zoom Phone license.
-			* Account owner or admin privileges 
-			
-			**Scopes:** `phone:write:admin`
-			
-			 **[Rate Limit Label](https://marketplace.zoom.us/docs/api-reference/rate-limits#rate-limits):** `Light`
-        """
-
-        # TBD
-        return
 
         res = self.request(
             'DELETE',
-            f'/phone/call_queues/{callQueueId}/policies/{policyType}'
+            f'/phone/call_queues/{call_queue_id}'
+        )
+
+        if res.status_code == 204:
+            return True
+        else:
+            raise Exception(res.json()['message'])
+
+    def get_call_queue(self, call_queue_id: str):
+        """
+            Call queues allow you to route incoming calls to a group of users. For instance, you can use call queues to route calls to various departments in your organization such as sales, engineering, billing, customer service etc.
+			 Use this API to get information on a specific Call Queue.
+			
+			 
+			**Prerequisites:**
+			
+			* Pro, Business, or Education account
+			* Account owner or admin permissions
+			* Zoom Phone license
+			
+			**Scopes:** `phone:read:admin`
+			 
+			
+			
+			 **[Rate Limit Label](https://marketplace.zoom.us/docs/api-reference/rate-limits#rate-limits):** `Light`
+        """
+
+
+        res = self.request(
+            'GET',
+            f'/phone/call_queues/{call_queue_id}'
         )
 
         return res.json()
-        
-        
+
+    def update_call_queue(self, call_queue_id: str, body: dict) -> bool:
+        """
+            Call queues allow you to route incoming calls to a group of users. For instance, you can use call queues to route calls to various departments in your organization such as sales, engineering, billing, customer service etc.
+			 Use this API to update information of a specific Call Queue.
+			 
+			**Prerequisites:**
+			
+			* Pro, Business, or Education account
+			* Account owner or admin permissions
+			* Zoom Phone license
+			
+			**Scopes:** `phone:write:admin`
+			 
+			
+			 **[Rate Limit Label](https://marketplace.zoom.us/docs/api-reference/rate-limits#rate-limits):** `Light`
+			
+        """
+
+
+        res = self.request(
+            'PATCH',
+            f'/phone/call_queues/{call_queue_id}',
+            json=body
+        )
+
+        if res.status_code == 204:
+            return True
+        else:
+            raise Exception(res.json()['message'])
